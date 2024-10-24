35abb0ae5bf177865685364d273806fb6ec58d70
happy2111 <muhammadyusufa2111@gmail.com>
2024-10-24 20:58:42 +0500

    List

---
+++ main.py
@@ -0,0 +1,97 @@
+# *************1
+
+# fruits = ["apples","banana", "cherry", "date", "elderberry"]
+#--- 1
+# fruits.remove("banana")
+# fruits.remove("date")
+
+
+#--- 2
+# fruits.pop(1) and fruits.pop(3)
+
+#--- 3 
+# del fruits[3]
+# del fruits[1]
+
+
+# print(fruits)
+
+
+# ************2
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# ---1
+# numbers.remove(2)
+# numbers.remove(4)
+# numbers.remove(6)
+# numbers.remove(8)
+# numbers.remove(10)
+# ---2
+# del number(1::2)
+# 1 - start   
+# 2 - nechtada bitta sakrash 
+
+
+
+# print(numbers)
+
+
+
+# ***********3
+
+
+# my_list = [10, 20, 30, 40, 50, 60, 70, 80]
+# # natija: [10, 20, 30, 70, 80]
+
+# firstPart = my_list[0:3]
+# secondPart = my_list[6:8]
+
+# firstPart.extend(secondPart)
+# print(firstPart)
+
+
+
+# **********4
+# cities = ["New York", "London", "Tokyo", "Moscow", "Paris"]
+# cities.pop(2)
+# print(cities)
+
+
+# *******5
+# numbers = [5,10,15,20,25]
+# del numbers[0]
+# ---1
+# del numbers[4]
+# print(numbers)
+
+# ---2
+# del fruits[-1]
+# del fruits[0]
+
+# *********6
+# fruits = ["apples","banana", "cherry", "date", "elderberry"]
+# ban =  fruits.pop(1)
+# print(ban)
+
+
+# **********7
+# items = [1,2,3,4,5]
+# del items[0:10]
+# print(items)
+
+
+# ************8
+# cars = ["Toyota", "Ford", "BMW", "Audi"]
+# ford = cars.pop(1)
+# ---1
+# cars.insert(100, ford)
+# ---2
+# cars.append(ford)
+# print(cars)
+
+
+# ****************9
+numbers = [1,2,3,4,5,6,7,8,9]
+numbers.reverse()
+print(numbers)
